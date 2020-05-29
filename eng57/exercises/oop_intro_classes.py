03e3edc9a0de5ef3ae3472484c0467c05ac13bcb
workwithstef <stefan.okolo@outlook.com>
2020-05-29 16:27:51 +0100

    my first Cat

---
+++ eng57/exercises/oop_intro_classes.py
@@ -0,0 +1,32 @@
+
+class Dog:
+    def bark(self):
+        return 'woof woof!'
+
+    def sleep(self):
+        return 'zzZZZZzzzZzzzzzzZZZz'
+
+
+class Cat:
+    def __init__(self, age, type='Tiger', name='Melon'):
+        self.name = name
+        self.age = age
+        self.type = type
+        self.colour = 'white with black stripes... or is it black with white stripes..?'
+        self.best_friend = 'Stefan'
+
+    def purr(self):
+        return '*vibrate* purrrrrr *vibrate*'
+
+    def bark(self):
+        return "What's that?"
+
+    def sleep(self):
+        return "You aint gotta tell me twice-- zzzzZZZzz"
+
+    def food(self):
+        return 'salmon'
+
+    def introduce_yourself(self, name, type):
+        pass
+
---
+++ eng57/exercises/oop_intro_objects.py
@@ -0,0 +1,5 @@
+from eng57.exercises.oop_intro_classes import *
+
+my_cat = Cat(3)
+print(my_cat.best_friend)
+print(my_cat.type)
